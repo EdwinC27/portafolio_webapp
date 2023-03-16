2e45a65042c4dcb10869507be36fe5a9fd80b7e2
Edwin Alejandro <Edwin_Alejandro@gmail.com>
2023-03-16 14:57:39 -0600

    Creacion de Desarrollador componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DesarrolladoresComponent } from './desarrolladores/desarrolladores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    component : DesarrolladoresComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
