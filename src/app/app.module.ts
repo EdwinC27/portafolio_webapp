d662c71a65895ce8772004e84028c23f518d4134
Edwin Alejandro <Edwin_Alejandro@gmail.com>
2023-03-16 15:13:21 -0600

    Actualizado de paquete y agregar Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DesarrolladoresComponent } from './desarrolladores/desarrolladores.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DesarrolladoresComponent } from './desarrolladores/desarrolladores.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
