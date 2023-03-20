8cc63190f064660530bf5a4b5184e4fba23c12e1
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-03-20 15:22:40 -0600

    Imagen de los proyectos

--- src/app/desarrolladores/desarrolladores.component.ts
+++ src/app/desarrolladores/desarrolladores.component.ts
@@ -10,42 +10,42 @@ export class DesarrolladoresComponent {
   projects: Project[] = [
     {
       title: 'Portafolio',
-      image: 'https://via.placeholder.com/200x150',
+      image: '../../assets/imagenes/portafolio.png',
       description: 'Portafolio personal ya funcional pero siguen con mejoras',
       deployedUrl: 'https://edwincastillo.netlify.app/',
       showDescription: false
     },
     {
       title: 'Diseño Web',
-      image: 'https://via.placeholder.com/200x150',
+      image: '../../assets/imagenes/diseñoWeb.png',
       description: 'Fue un proyecto para ver cómo sería mi portafolio, pero hay que aclarar que no es funcional es puro diseño para escritorio.',
       deployedUrl: 'https://pre-presentacion.netlify.app/',
       showDescription: false
     },
     {
       title: 'Home Study',
-      image: 'https://via.placeholder.com/200x150',
+      image: '../../assets/imagenes/homeStudy.png',
       description: 'Este proyecto realizado es un prototipo, de una aplicación que este dirigida en el aprendizaje de las personas, sin importar su conocimiento o experiencia en el tema. Asimismo seria 100% educacion Online.      ',
       deployedUrl: 'https://home-study.netlify.app/',
       showDescription: false
     },
     {
       title: 'Tienda de Camisas',
-      image: 'https://via.placeholder.com/200x150',
+      image: '../../assets/imagenes/tiendaCamisa.png',
       description: 'Diseño de una tienda de camisa para programadores, lo interezante es que las camizas tienen estampados de Tecnologia',
       deployedUrl: 'https://venta-camisas.netlify.app/',
       showDescription: false
     },
     {
       title: 'Microcontrolador',
-      image: 'https://via.placeholder.com/200x150',
+      image: 'https://via.placeholder.com/200x100',
       description: 'Aceptar solicitudes RESTful que reciba como parámetro el nombre de la ciudad o las coordenadas largas de latitud y devuelva una sugerencia de lista de reproducción de acuerdo con la temperatura.',
       deployedUrl: 'https://github.com/EdwinC27/Microcontrolador',
       showDescription: false
     },
     {
       title: 'Informacion IPs',
-      image: 'https://via.placeholder.com/200x150',
+      image: '../../assets/imagenes/IPs.png',
       description: 'Ver información de una IP, ya sea ipv4 o ipv6',
       deployedUrl: 'https://ipinf.netlify.app/',
       showDescription: false
