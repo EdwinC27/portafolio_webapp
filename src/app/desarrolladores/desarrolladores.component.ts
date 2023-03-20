a65bb5d008a2252d37a8c597fad928ea0512e849
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-03-19 20:47:42 -0600

    Agregar pryectos al portaforlio

--- src/app/desarrolladores/desarrolladores.component.ts
+++ src/app/desarrolladores/desarrolladores.component.ts
@@ -9,47 +9,47 @@ import { Project } from './desarrolladores.model';
 export class DesarrolladoresComponent {
   projects: Project[] = [
     {
-      title: 'Proyecto 1',
+      title: 'Portafolio',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 1',
-      deployedUrl: 'https://ejemplo.com/proyecto-1',
+      description: 'Portafolio personal ya funcional pero siguen con mejoras',
+      deployedUrl: 'https://edwincastillo.netlify.app/',
       showDescription: false
     },
     {
-      title: 'Proyecto 2',
+      title: 'Diseño Web',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 2',
-      deployedUrl: 'https://ejemplo.com/proyecto-2',
+      description: 'Fue un proyecto para ver cómo sería mi portafolio, pero hay que aclarar que no es funcional es puro diseño para escritorio.',
+      deployedUrl: 'https://pre-presentacion.netlify.app/',
       showDescription: false
     },
     {
-      title: 'Proyecto 3',
+      title: 'Home Study',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 3',
-      deployedUrl: 'https://ejemplo.com/proyecto-3',
+      description: 'Este proyecto realizado es un prototipo, de una aplicación que este dirigida en el aprendizaje de las personas, sin importar su conocimiento o experiencia en el tema. Asimismo seria 100% educacion Online.      ',
+      deployedUrl: 'https://home-study.netlify.app/',
       showDescription: false
     },
     {
-      title: 'Proyecto 4',
+      title: 'Tienda de Camisas',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 4',
-      deployedUrl: 'https://ejemplo.com/proyecto-4',
+      description: 'Diseño de una tienda de camisa para programadores, lo interezante es que las camizas tienen estampados de Tecnologia',
+      deployedUrl: 'https://venta-camisas.netlify.app/',
       showDescription: false
     },
     {
-      title: 'Proyecto 5',
+      title: 'Microcontrolador',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 5',
-      deployedUrl: 'https://ejemplo.com/proyecto-5',
+      description: 'Aceptar solicitudes RESTful que reciba como parámetro el nombre de la ciudad o las coordenadas largas de latitud y devuelva una sugerencia de lista de reproducción de acuerdo con la temperatura.',
+      deployedUrl: 'https://github.com/EdwinC27/Microcontrolador',
       showDescription: false
     },
     {
-      title: 'Proyecto 6',
+      title: 'Informacion IPs',
       image: 'https://via.placeholder.com/200x150',
-      description: 'Descripción del proyecto 6',
-      deployedUrl: 'https://ejemplo.com/proyecto-6',
+      description: 'Ver información de una IP, ya sea ipv4 o ipv6',
+      deployedUrl: 'https://ipinf.netlify.app/',
       showDescription: false
-    }
+    },
   ];
   
   selectedProject: Project | null = null;
