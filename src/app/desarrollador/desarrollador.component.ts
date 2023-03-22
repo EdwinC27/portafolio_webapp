ca1b64bb13d45a38f6262132fefe71b2bda55bff
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-03-21 21:00:32 -0600

    Poner el texto de la presentacion en variables

--- src/app/desarrollador/desarrollador.component.ts
+++ src/app/desarrollador/desarrollador.component.ts
@@ -7,6 +7,10 @@ import { Certificados, Project } from './desarrollador.model';
   styleUrls: ['./desarrollador.component.css']
 })
 export class DesarrolladorComponent {
+  presentacionTitle:string = "Hola, soy Edwin Castillo, un apasionado del desarrollo y gestión de software";
+
+  presentacionText:string = "Tengo actualmente 18 años, soy un apacionado de la tecnología, me gusta aprender constantemente cosas nuevas relacionado con la programación, mi principal pasatiempo es la seguridad informática y el desarrollo de software."
+
   projects: Project[] = [
     {
       title: 'Portafolio',
