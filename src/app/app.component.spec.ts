91c38a6ce8d8bb4744d72f21631714bfafc689c8
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-03-30 22:53:23 -0600

    Agregar un Test para checar el lenguaje

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -16,17 +16,25 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  it('should use default language if browser language does not match a supported language', () => {
+    spyOn(TranslateService.prototype, 'getBrowserLang').and.returnValue('fr');
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const translateService = TestBed.inject(TranslateService);
+    expect(translateService.currentLang).toEqual('en');
+  });
+
+
+  it('should add supported languages', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const translateService = fixture.debugElement.injector.get(TranslateService);
+    expect(translateService.getLangs()).toEqual(['en', 'es']);
+  });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PORTAFOLIO app is running!');
-  });
 });
